265a79095152e74d93be3b90dd6348ae7145a89c
Eviatar <eviatarmor8@gmail.com>
2021-02-12 20:29:26 -0500

    Updated to more readable code.

--- SafeEndianUnion.hpp
+++ SafeEndianUnion.hpp
@@ -115,38 +115,41 @@ static constexpr T& get_by_type(UnionImpl<Ts...>& u)
 #else
 
 template<typename... Ts>
-struct UnionImpl 
+class UnionImpl 
 {
+public:
+
+    template<typename T>
+    constexpr void set_data(const T& value)
+    {
+        static_assert(std::disjunction_v<std::is_same<T, Ts>...>, "T does not exists in the union.");
+        data = std::bit_cast<decltype(data)>(value);
+    }
+
+    template<size_t i>
+    [[nodiscard]]
+    constexpr auto get_by_index()
+    {
+        static_assert(i < sizeof...(Ts), "index is too big!");
+	    using element_t = typename std::tuple_element_t<i, std::tuple<Ts...>>;
+	    return std::bit_cast<element_t>(data);
+    }
+
+    template<typename T>
+    [[nodiscard]]
+    constexpr auto get_by_type()
+    {
+        static_assert(std::disjunction_v<std::is_same<T, Ts>...>, "T does not exists in the union.");
+	    return std::bit_cast<T>(data);
+    }
+
+private:
 	static constexpr size_t data_size = std::max({sizeof(Ts)...});
 	// using data_t = typename std::aligned_union_t<data_size, Ts...>;
 
-        using data_t = std::array<std::byte, data_size>;
+    using data_t = std::array<std::byte, data_size>;
 	data_t data;
 };
-
-template<typename T, typename... Ts>
-static constexpr void set_data(const T& value, UnionImpl<Ts...>& u) 
-{
-    static_assert(std::disjunction_v<std::is_same<T, Ts>...>, "T does not exists in the union.");
-    u.data = std::bit_cast<decltype(u.data)>(value);
-}
-
-template<size_t i, typename... Ts>
-[[nodiscard]]
-static constexpr auto get_by_index(UnionImpl<Ts...>& u)
-{
-	static_assert(i < sizeof...(Ts), "index is too big!");
-	using element_t = typename std::tuple_element_t<i, std::tuple<Ts...>>;
-	return std::bit_cast<element_t>(u.data);
-}
-
-template<typename T, typename... Ts>
-[[nodiscard]]
-static constexpr T get_by_type(UnionImpl<Ts...>& u)
-{
-	static_assert(std::disjunction_v<std::is_same<T, Ts>...>, "T does not exists in the union.");
-	return std::bit_cast<T>(u.data);
-}
 
 #endif
 
@@ -181,14 +184,26 @@ template<typename T>
 static constexpr bool is_bounded_array_v = is_bounded_array<T>::value;
 
 // -------------------------------------------------------------------------
+// Remove pointer / reference.
+template<typename T>
+struct remove_pr
+{
+    using type = typename std::remove_pointer_t<
+        typename std::remove_reference_t<T>>;
+};
+
+template<typename T>
+using remove_pr_t = typename remove_pr<T>::type;
+
+// -------------------------------------------------------------------------
 // Checks if type a is NOT cv ( const / volatile ) and it's not a pointer / reference.
 template<typename T>
 struct is_plain_type 
 {
 	static constexpr bool value = 
 		std::is_same_v<
-			std::remove_const_t<std::remove_volatile_t<
-					std::remove_pointer_t<std::remove_reference_t<T>>>>, 
+            std::remove_cv_t<
+				remove_pr_t<T>>, 
 		T>;
 };
 
@@ -237,12 +252,12 @@ static constexpr bool is_possible_type_in_struct_v = is_possible_type_in_struct<
 // - 32 bytes
 // - 64 bytes
 // - Others may cause compile-time errors.
-class swap_endian
+class SwapEndian
 {
 private:
 	template<typename T>
 	[[nodiscard]]
-	static inline T byte_order_swap(T value)
+	static constexpr T byte_order_swap(T value)
 		requires ( sizeof(T) == sizeof(uint8_t) )
 	{
 		// Reversing the bits.
@@ -255,7 +270,7 @@ private:
 	
 	template<typename T>
 	[[nodiscard]]
-	static inline T byte_order_swap(T value)
+	static constexpr T byte_order_swap(T value)
 		requires ( sizeof(T) == sizeof(uint16_t) && std::is_integral_v<T> )
 	{
 #if defined(_MSC_VER)
@@ -269,7 +284,7 @@ private:
 
 	template<typename T>
 	[[nodiscard]]
-	static inline T byte_order_swap(T value)
+	static constexpr T byte_order_swap(T value)
 		requires ( sizeof(T) == sizeof(uint32_t) && std::is_integral_v<T> )
 	{
 #if defined(_MSC_VER)
@@ -286,7 +301,7 @@ private:
 
 	template<typename T>
 	[[nodiscard]]
-	static inline T byte_order_swap(T value)
+	static constexpr T byte_order_swap(T value)
 		requires ( sizeof(T) == sizeof(uint64_t) && std::is_integral_v<T> )
 	{
 #if defined(_MSC_VER)
@@ -294,7 +309,7 @@ private:
 #elif defined(__GNUC__) || defined(__clang__)
 		return __builtin_bswap64(value);
 #else
-		return ( value >> 56) 			    |
+		return ( value >> 56)                       |
 		       ((value << 40) & 0x00FF000000000000) |
 		       ((value << 24) & 0x0000FF0000000000) |
 		       ((value <<  8) & 0x000000FF00000000) |
@@ -307,7 +322,7 @@ private:
 
 	template<typename T>
 	[[nodiscard]]
-	static inline T byte_order_swap(T value)
+	static constexpr T byte_order_swap(T value)
 		requires ( sizeof(float) == sizeof(uint32_t) && std::is_floating_point_v<T> )
 	{
 		uint32_t temp;
@@ -320,7 +335,7 @@ private:
 
 	template<typename T>
 	[[nodiscard]]
-	static inline T byte_order_swap(T value)
+	static constexpr T byte_order_swap(T value)
 		requires ( sizeof(double) == sizeof(uint64_t) && std::is_floating_point_v<T> )
 	{
 		uint64_t temp;
@@ -333,14 +348,14 @@ private:
 
 	template<typename T>
 	[[nodiscard]] [[noreturn]]
-	static inline T byte_order_swap(T value) {
+	static constexpr T byte_order_swap(T value) {
 		static_assert(std::is_same_v<T, void>, "System has unknown size.");
 	}
 
 public:
 	template<typename T>
 	[[nodiscard]]
-	static inline T swap(const T& value)
+	static constexpr T swap(const T& value)
 		requires( std::is_arithmetic_v<T> )
 	{
 		return byte_order_swap(value);
@@ -348,7 +363,7 @@ public:
 
 	template<typename T>
 	[[nodiscard]]
-	static inline T swap(const T& src)
+	static constexpr T swap(const T& src)
 		// requires data structure or array
 	{
 		T dest;
@@ -361,9 +376,9 @@ public:
 	}
 };
 
-#ifdef EVI_ENABLE_REFLECTION_SYSTEM
 // -------------------------------------------------------------------------
 // Simple reflection system to validate the types inside a struct.
+#ifdef EVI_ENABLE_REFLECTION_SYSTEM
 
 // Class is convertible to anything.
 struct UniversalType
@@ -376,7 +391,7 @@ struct UniversalType
 // NOTE: This only works for aggregate types.
 template<typename T, typename... Ts>
 // can be also consteval
-constexpr auto count_member_fields(Ts... members)
+static consteval auto count_member_fields(Ts... members)
 {
 	if constexpr( requires { T{members...}; } == false )
 		return sizeof...(members) - 1;
@@ -389,13 +404,13 @@ template<size_t size, typename T>
 struct StructToTuple;
 
 #define __EVI_MAKE_STRUCT_TO_TUPLE_SPECIALIZATION(STRUCT, FIELDS_NUM, ...) \
-	template<typename T> 						   \
-	struct STRUCT<FIELDS_NUM, T> { 					   \
-		constexpr static auto unevaluated(T& u) 		   \
-		{							   \
-			auto&& [__VA_ARGS__] = u;			   \
-			return std::tuple{__VA_ARGS__};			   \
-		}							   \
+	template<typename T>                                                   \
+	struct STRUCT<FIELDS_NUM, T> {                                         \
+		static constexpr auto unevaluated(T& u) noexcept                   \
+		{                                                                  \
+			auto&& [__VA_ARGS__] = u;                                      \
+			return std::tuple{__VA_ARGS__};                                \
+		}                                                                  \
 	}
 
 __EVI_MAKE_STRUCT_TO_TUPLE_SPECIALIZATION(StructToTuple, 2 , m1, m2);
@@ -432,25 +447,28 @@ __EVI_MAKE_STRUCT_TO_TUPLE_SPECIALIZATION(StructToTuple, 32, m1, m2, m3, m4, m5,
 
 // Checking all of the members in a tuple to validate them.
 template<typename T, typename... Ts>
-static constexpr bool check_tuple_types(std::tuple<T, Ts...>*) 
+static consteval bool check_tuple_types(std::tuple<T, Ts...>*) 
 {
 	constexpr bool first_member = is_possible_type_in_struct_v<T>;
 	constexpr bool rest_members = std::conjunction_v<is_possible_type_in_struct<Ts>...>;
 	constexpr bool all_same     = std::conjunction_v<std::is_same<T, Ts>...>;
+
+    static_assert(first_member || rest_members, "Your struct fields are having an incorrect type.");
+    static_assert(all_same, "Your struct fields are not the same.");
 
 	return first_member && rest_members && all_same;
 }
 
 // Converting the struct to tuple and validating it.
 template<typename T>
-constexpr bool validate_possible_structs()
+consteval bool validate_possible_structs()
 	requires ( std::is_class_v<T> )
 {
 	constexpr auto size = count_member_fields<T>();
 	if constexpr(size >= 2)
 	{
 		using U = std::invoke_result_t<decltype(StructToTuple<size, T>::unevaluated), T&>;
-		return check_tuple_types((U*)nullptr);
+        return check_tuple_types(static_cast<U*>(nullptr));
 	}
 	
 	return true;
@@ -459,7 +477,7 @@ constexpr bool validate_possible_structs()
 #endif // EVI_ENABLE_REFLECTION_SYSTEM
 
 template<typename T>
-constexpr bool validate_possible_structs() {
+consteval bool validate_possible_structs()  noexcept {
 	return true;
 }
 
@@ -508,7 +526,7 @@ private:
 		if constexpr(endian != std::endian::native)
 		{
 			if(m_type_code != typeid(T).hash_code())
-				ret = detail::swap_endian::swap(value);
+				ret = detail::SwapEndian::swap(value);
 		}
 
 		return ret;
@@ -520,9 +538,9 @@ private:
 		m_type_code = typeid(T).hash_code();
 	
 		if constexpr(detail::is_struct_standard_layout_v<T> || detail::is_bounded_array_v<T>)
-			set_data(check_and_fix_endianness(value), this->m_union);
+			this->m_union.set_data(check_and_fix_endianness(value));
 		else
-			set_data(value, this->m_union);
+            this->m_union.set_data(value);
 	}
 
 public:
@@ -556,14 +574,14 @@ public:
 	template<size_t i>
 	constexpr auto get()
 	{
-		auto value = detail::get_by_index<i>(this->m_union);
+		const auto value = this->m_union. template get_by_index<i>();
 		return check_and_fix_endianness(value);
 	}
 
 	template<typename T>
 	constexpr auto get()
 	{
-		T value = detail::get_by_type<T>(this->m_union);
+		const T value = this->m_union. template get_by_type<T>();
 		return check_and_fix_endianness(value);
 	}
 
